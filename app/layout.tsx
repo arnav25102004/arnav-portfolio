93c65e67c187994d1e06a98b39e3687c30b664b3
Arnav <arnavnarula25@gmail.com>
2026-06-14 00:45:40 +0530

    style: add marquee, glow, glass animations and loading screen to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import PageTransition from "@/components/PageTransition";
+import LoadingScreen from "@/components/LoadingScreen";
 import { personalInfo } from "@/lib/data";
 
 const syne = Syne({
@@ -127,6 +128,7 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen flex flex-col font-sans antialiased">
+        <LoadingScreen />
         <ThemeProvider>
           <Navbar />
           <PageTransition>
